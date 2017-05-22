4effeb3fe4f04577566ceec55f248e7e92fef84c
marijnburger <marijnburger@pcp077753pcs.wireless.calpoly.edu>
2017-05-21 19:13:06 -0700

    add bare paragraphs

--- app/scripts/articleGenerator/run.js
+++ app/scripts/articleGenerator/run.js
@@ -9,9 +9,28 @@ function randomSelect(values) {
 
 function run() {
     var ID = 1;
+
+    // == begin page, build navbar =============================================
     var page = '';
     page += build.navbar(ID);
+
     page += '<div id="page-content-' + ID + '">' //begin page content
+
+    // == build body text ======================================================
+    var wordCount = randomSelect(values.attributeValues.wordCount);
+    console.log(wordCount);
+    var paragraphs = new Array();
+    var createdWordCount = 0;
+    var returned;
+    //build paragraphs until wordCount is exceeded
+    for (var iteration = 1; createdWordCount < wordCount; iteration++) {
+        returned = build.paragraph(ID, iteration, 0);
+        createdWordCount += returned.wordCount;
+        paragraphs.push(returned.paragraph);
+    }
+    paragraphs.forEach(function(value) {
+        page += value;
+    });
 
     page += '</div>' //end page content
     console.log(page);
