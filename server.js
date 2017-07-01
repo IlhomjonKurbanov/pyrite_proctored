368897c573133bfeea3516a1e2667dacfd1449ec
marijnburger <marijnburger@Marijns-MacBook-Pro.local>
2017-06-30 22:02:31 -0700

    fixed spacing

--- server.js
+++ server.js
@@ -4,9 +4,9 @@
 
 // == set up ===================================================================
 var express        = require('express');
-var app            = express();                  // create our app w/ express
-var morgan         = require('morgan');          // log requests to the console (express4)
-var bodyParser     = require('body-parser');     // pull information from HTML POST (express4)
+var app            = express();              //create our app w/ express
+var morgan         = require('morgan');      //log requests to the console (express4)
+var bodyParser     = require('body-parser'); //pull information from HTML POST (express4)
 var port           = 80;
 
 // == configuration ============================================================
@@ -19,13 +19,14 @@ db.connect(function(err) {
     }
 });
 
-app.use(express.static(__dirname + '/public'));                 // set static files location, /public/server.js is now /server.js
-app.use(express.static(__dirname + '/app'));                    // set additional static
-app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.json());                                     // parse application/json
+app.use(express.static(__dirname + '/public')); //set static files location, /public/server.js is now /server.js
+app.use(express.static(__dirname + '/app'));    //set additional static
+app.use(morgan('dev'));                         //log every request to the console
+app.use(bodyParser.json());                     //parse application/json
 
 // == routes ===================================================================
-require('./app/routes')(app, db); // pass our application and DB connection into our routes
+//pass our application, DB connection, and videoshow plugin into our routes
+require('./app/routes')(app, db, videoshow);
 
 // == listen (start app with node server.js) ===================================
 var d = new Date();
