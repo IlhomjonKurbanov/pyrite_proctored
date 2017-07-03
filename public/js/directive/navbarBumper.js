603e75f65add62feda750be55928a8ff800c808c
marijnburger <marijnburger@Marijns-MacBook-Pro.local>
2017-07-02 19:28:23 -0700

    fixed bug with fixed nabbers

--- public/js/directive/navbarBumper.js
+++ public/js/directive/navbarBumper.js
@@ -7,10 +7,11 @@
 // when the navbar is flipped to fixed position to act as an upper bumper for the
 // article text content
 angular.module('pyrite')
-    .directive('navbarBumper', function () {
+    .directive('navbarBumper', function ($location) {
         return {
             restrict: 'A',
             link: function(scope, element, attr) {
+                if ($location.path() == '/review') return;
                 element.css('height', element.css('height'));
             }
         };
--- public/js/directive/navbarFixOnScroll.js
+++ public/js/directive/navbarFixOnScroll.js
@@ -4,10 +4,15 @@
 // ------------------------------------------------
 // fixes element width and applies 'navbar-fixed' class when scroll reaches given offset
 angular.module('pyrite')
-    .directive('navbarFixOnScroll', function ($window) {
+    .directive('navbarFixOnScroll', function ($window, $location) {
         return {
             restrict: 'A',
             link: function(scope, element, attr) {
+                if ($location.path() == '/review') {
+                    element.removeClass('navbar-fixed');
+                    element.removeAttr('navbar-fix-on-scroll');
+                    return;
+                }
                 element.css('width', element.css('width'));
                 var top = element.offset().top;
                 angular.element($window).bind("scroll", function() {
