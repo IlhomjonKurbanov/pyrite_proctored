a6697ec421677e0c3527a46805143622870bed46
marijnburger <marijnburger@users.noreply.github.com>
2017-07-19 14:53:28 -0700

    add 'restart' button

--- public/js/controller/endController.js
+++ public/js/controller/endController.js
@@ -3,8 +3,16 @@
 // controller for 'end' view
 
 angular.module('pyrite')
-    .controller('endController', ['$scope', 'cookieService',
-        function($scope, cookieService) {
-            //setup page variables
+    .controller('endController', ['$scope', '$location', 'cookieService', 'progressService',
+        function($scope, $location, cookieService, progressService) {
             $scope.subjectID = cookieService.getSubjectID() || 'undefined';
+
+            $scope.restart = function() {
+                //clean up and restart
+                cookieService.cleanUp(['subjectID', 'articleOrder', 'progress']);
+
+                progressService.setStage('unstarted');
+                progressService.setArticleIndex('demo1');
+                $location.path('/');
+            }
         }]);
