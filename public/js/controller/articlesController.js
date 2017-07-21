e64a8a89d89399149620593febe65bc532d6e5a4
marijnburger <marijnburger@users.noreply.github.com>
2017-07-21 15:12:55 -0700

    remove halfway point modal, dev and production

--- public/js/controller/articlesController.js
+++ public/js/controller/articlesController.js
@@ -34,13 +34,6 @@ angular.module('pyrite')
                 $scope.articleOrder = $rootScope.articleOrder || cookieService.getArticleOrder();
                 $scope.articlePath = $scope.articleOrder[$scope.index]; //get articlePath
                 $scope.articleID = $scope.articlePath.split('_')[1].split('.')[0];
-
-                //if at halfway point, show halfway modal
-                if ($scope.index == Math.round($scope.articleOrder.length / 2) - 1) {
-                    setTimeout(function () {
-                        $("#halfway").modal("show");
-                    }, 0);
-                }
             }
 
             //set page time variable
